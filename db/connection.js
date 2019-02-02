2d03bd40271642836108b30fb8573072f574f694
da Vinci <brucewilson1103@gmail.com>
2019-02-02 11:57:24 -0500

    initial files

---
+++ db/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+require('dotenv').config();
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "noteTakerDB"
+  });
+}
+
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
+  }
+  return next();
+};
+
+module.exports = connection;
